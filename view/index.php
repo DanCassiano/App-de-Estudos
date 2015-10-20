47896fd0301e24fbb81b66cc56196c946d2acd82
DanCassiano <jordan_gnr@hotmail.com>
2015-10-19 22:55:02 -0200

    Nova font e novo CSS

--- view/index.php
+++ view/index.php
@@ -3,20 +3,46 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php echo $this->getTitulo() ?></title>
-	<link rel="stylesheet" href="view/assets/css/app.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="<?php echo BASE_URL ?>/view/assets/css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" href="<?php echo BASE_URL ?>/view/assets/css/app.css">
 </head>
 <body class="expandido">	
 	<div class="container-fixo">
 		<div class="nav nav-default fixo-top">
 			<div class="container-nav">
-				<a href="#" class="btn" id="btnMenu"><span class="icone menu"></span></a>
+				<a href="#" class="btn" id="btnMenu"><i class="zmdi zmdi-more-vert zmdi-hc-2x cor-padrao"></i></a>
+				<div class="painel painel-right">
+					<img src="" alt="">
+				</div>
 			</div>
 		</div>
-		<div class="barra fixa left barra-default">
-			
+		<div class="barra fixa barra-default barra-menu">
+			<?php 
+				$ativo = Request::get('query');
+				$menu1 = "";
+				$menu2 = "";
+				 switch ($ativo) {
+				 	case 'calendario':
+				 		$menu1 = "active";
+				 		break;
+				 	case 'config':
+				 		$menu2 = "active";
+				 		break;
+				 	
+				 }
+			 ?>
+			<ul class="menu menu-app">
+				<li class="<?php echo $menu1 ?>">
+					<a href="<?php echo BASE_URL ?>/app-admin/calendario"><i class="zmdi zmdi-view-dashboard"></i>calendário</a>
+				</li>
+				<li class="<?php echo $menu2 ?>">
+					<a href="<?php echo BASE_URL ?>/app-admin/config"><i class="zmdi zmdi-settings"></i>configurações</a>
+				</li>				
+			</ul>
 		</div>
 	</div>
-	<script src="view/assets/js/jquery.js"></script>
-	<script src="view/assets/js/app.js"></script>
+	<script src="<?php echo BASE_URL ?>/view/assets/js/jquery.js"></script>
+	<script src="<?php echo BASE_URL ?>/view/assets/js/app.js"></script>
 </body>
 </html>
