57133f40d6c7e7380b5341e446efea981cd3ce9a
DanCassiano <jordan_gnr@hotmail.com>
2015-10-21 21:51:25 -0200

    Atualizacoes e adicao de controle de pagina

--- view/index.php
+++ view/index.php
@@ -6,8 +6,10 @@
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="<?php echo BASE_URL ?>/view/assets/css/material-design-iconic-font.min.css">
 	<link rel="stylesheet" href="<?php echo BASE_URL ?>/view/assets/css/app.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL ?>/view/assets/css/jquery-ui.min.css">
+	<base href="<?php echo BASE_URL ?>/" />
 </head>
-<body class="expandido">	
+<body class="">	
 	<div class="container-fixo">
 		<div class="nav nav-default fixo-top">
 			<div class="container-nav">
@@ -22,27 +24,53 @@
 				$ativo = Request::get('query');
 				$menu1 = "";
 				$menu2 = "";
-				 switch ($ativo) {
+				$menu3 = "";
+
+				switch ($ativo) {
 				 	case 'calendario':
 				 		$menu1 = "active";
 				 		break;
-				 	case 'config':
+				 	case 'mail':
 				 		$menu2 = "active";
 				 		break;
-				 	
-				 }
-			 ?>
+				 	case 'config':
+				 		$menu3 = "active";
+				 		break;				 	
+				}
+			?>
 			<ul class="menu menu-app">
 				<li class="<?php echo $menu1 ?>">
 					<a href="<?php echo BASE_URL ?>/app-admin/calendario"><i class="zmdi zmdi-view-dashboard"></i>calendário</a>
 				</li>
 				<li class="<?php echo $menu2 ?>">
+					<a href="<?php echo BASE_URL ?>/app-admin/mail"><i class="zmdi zmdi-mail-send"></i>mail</a>
+				</li>
+				<li class="<?php echo $menu3 ?>">
 					<a href="<?php echo BASE_URL ?>/app-admin/config"><i class="zmdi zmdi-settings"></i>configurações</a>
 				</li>				
 			</ul>
 		</div>
+		<div class="conteudo">
+			<?php 
+				$include = "";
+				switch ($ativo) {
+				 	case 'calendario':
+				 		
+				 		break;
+				 	case 'mail':
+				 		$include = "mail.php";
+				 		break;
+				 	case 'config':				 		
+				 		break;				 	
+				}
+
+				include $include;
+			?>
+		</div>
 	</div>
 	<script src="<?php echo BASE_URL ?>/view/assets/js/jquery.js"></script>
+	<script src="<?php echo BASE_URL ?>/view/assets/js/jquery-ui.min.js"></script>
 	<script src="<?php echo BASE_URL ?>/view/assets/js/app.js"></script>
+	<script src="<?php echo BASE_URL ?>/view/assets/js/mail.js"></script>
 </body>
 </html>
