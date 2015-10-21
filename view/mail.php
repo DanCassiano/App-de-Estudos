ef53f018f45c8beb6b849131b109b05052d567a1
DanCassiano <jordan_gnr@hotmail.com>
2015-10-21 21:49:33 -0200

    Pagina do email

---
+++ view/mail.php
@@ -0,0 +1,25 @@
+<div class="e-mail">
+	<div class="row text-center">
+		<div class="row-container ">
+			<div class="img">
+				<img src="view/assets/imagem/placeholder_01.png" height="280" width="500">
+			</div>
+		</div>
+		<a class="row-handle" href="#"><i class="zmdi zmdi-arrows"></i></a>
+		<a class="row-del"><i class="zmdi zmdi-delete"></i></a>
+		<a class="row-dupl"><i class="zmdi zmdi-collection-item"></i></a>
+	</div>
+
+	<div class="row text-center">
+		<div class="row-container ">
+			<img src="view/assets/imagem/placeholder_01.png" height="280" width="500">
+		</div>
+		<a class="row-handle" href="#"><i class="zmdi zmdi-arrows"></i></a>
+		<a class="row-del"><i class="zmdi zmdi-delete"></i></a>
+		<a class="row-dupl"><i class="zmdi zmdi-collection-item"></i></a>
+	</div>
+
+	
+
+</div>
+
