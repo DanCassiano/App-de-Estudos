6bc5ee5f9dc7a54d655c18caf2608199bf7161ad
DanCassiano <jordan_gnr@hotmail.com>
2015-10-25 15:43:22 -0200

    Adicionando o mousDown
    Evento do MouseDown para slide

--- view/assets/js/app.js
+++ view/assets/js/app.js
@@ -128,111 +128,59 @@
 
 
 	var SlidePin = function(){
-
+		var sp 	  = this,
+			slide = null;
+		$(function(){
+			sp._init();
+		});		
 	}
 
 	var slidepin = SlidePin.prototype;
 
 		slidepin._init = function(){
-			document.onmousedown = OnMouseDown;
-    		document.onmouseup = OnMouseUp;
+				var sp = this;
+				
+				sp._initDragDrop();
+
+				var slideElement = $("[data-app='slide-pin'], .slide-pin");
+
+					slideElement.each(function(i,d){
+						sp._dragElement = $(d);
+						$(d).on('click', 'a' , function(e){
+							e.preventDefault();
+						})
+					});
 		}
+
+		slidepin._onMouseDown = function(e){
+
+			console.log( e.target, e.target.className )
+
+			if( e.target.className == 'slide-pin')
+		}
+
+		slidepin._onMouseUp = function(e){
+
+		}
+
+		slidepin._onMouseMove = function(e){
+
+		}
+
+
+		slidepin._initDragDrop = function(){
+			document.onmousedown = this._onMouseDown;
+    		document.onmouseup = this._onMouseUp;
+    		document.onmousemove = this._onMouseMove;
+		}	
 
 	window.slidepin = new SlidePin();
 
-
-	function OnMouseDown(e)
-	{
-	    // IE is retarded and doesn't pass the event object
-	    if (e == null) 
-	        e = window.event; 
-	    
-	    // IE uses srcElement, others use target
-	    var target = e.target != null ? e.target : e.srcElement;
-	    
-	    _debug.innerHTML = target.className == 'drag' 
-	        ? 'draggable element clicked' 
-	        : 'NON-draggable element clicked';
-
-	    // for IE, left click == 1
-	    // for Firefox, left click == 0
-	    if ((e.button == 1 && window.event != null || 
-	        e.button == 0) && 
-	        target.className == 'drag')
-	    {
-	        // grab the mouse position
-	        _startX = e.clientX;
-	        _startY = e.clientY;
-	        
-	        // grab the clicked element's position
-	        _offsetX = ExtractNumber(target.style.left);
-	        _offsetY = ExtractNumber(target.style.top);
-	        
-	        // bring the clicked element to the front while it is being dragged
-	        _oldZIndex = target.style.zIndex;
-	        target.style.zIndex = 10000;
-	        
-	        // we need to access the element in OnMouseMove
-	        _dragElement = target;
-
-	        // tell our code to start moving the element with the mouse
-	        document.onmousemove = OnMouseMove;
-	        
-	        // cancel out any text selections
-	        document.body.focus();
-
-	        // prevent text selection in IE
-	        document.onselectstart = function () { return false; };
-	        // prevent IE from trying to drag an image
-	        target.ondragstart = function() { return false; };
-	        
-	        // prevent text selection (except IE)
-	        return false;
-	    }
-	}
-
-	function OnMouseMove(e)
-	{
-	    if (e == null) 
-	        var e = window.event; 
-
-	    // this is the actual "drag code"
-	    _dragElement.style.left = (_offsetX + e.clientX - _startX) + 'px';
-	    _dragElement.style.top = (_offsetY + e.clientY - _startY) + 'px';
-	    
-	    _debug.innerHTML = '(' + _dragElement.style.left + ', ' + 
-	        _dragElement.style.top + ')';   
-	}
-
-	function OnMouseUp(e)
-	{
-	    if (_dragElement != null)
-	    {
-	        _dragElement.style.zIndex = _oldZIndex;
-
-	        // we're done with these events until the next OnMouseDown
-	        document.onmousemove = null;
-	        document.onselectstart = null;
-	        _dragElement.ondragstart = null;
-
-	        // this is how we know we're not dragging      
-	        _dragElement = null;
-	        
-	        _debug.innerHTML = 'mouse up';
-	    }
-	}
-
 	function ExtractNumber(value)
 	{
-	    var n = parseInt(value);
-		
-	    return n == null || isNaN(n) ? 0 : n;
+    	var n = parseInt(value);	
+    	return n == null || isNaN(n) ? 0 : n;
 	}
 
-	// this is simply a shortcut for the eyes and fingers
-	function $(id)
-	{
-	    return document.getElementById(id);
-	}
 
 })(window,jQuery);
