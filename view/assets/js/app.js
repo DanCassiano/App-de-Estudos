760756a6e0dad8c8fb2840f7011be392371dff03
DanCassiano <jordan_gnr@hotmail.com>
2015-10-25 13:08:06 -0200

    Slide pin
    Plugin para slide simples

--- view/assets/js/app.js
+++ view/assets/js/app.js
@@ -126,4 +126,113 @@
 		}
 	window.slide = new Slide();
 
+
+	var SlidePin = function(){
+
+	}
+
+	var slidepin = SlidePin.prototype;
+
+		slidepin._init = function(){
+			document.onmousedown = OnMouseDown;
+    		document.onmouseup = OnMouseUp;
+		}
+
+	window.slidepin = new SlidePin();
+
+
+	function OnMouseDown(e)
+	{
+	    // IE is retarded and doesn't pass the event object
+	    if (e == null) 
+	        e = window.event; 
+	    
+	    // IE uses srcElement, others use target
+	    var target = e.target != null ? e.target : e.srcElement;
+	    
+	    _debug.innerHTML = target.className == 'drag' 
+	        ? 'draggable element clicked' 
+	        : 'NON-draggable element clicked';
+
+	    // for IE, left click == 1
+	    // for Firefox, left click == 0
+	    if ((e.button == 1 && window.event != null || 
+	        e.button == 0) && 
+	        target.className == 'drag')
+	    {
+	        // grab the mouse position
+	        _startX = e.clientX;
+	        _startY = e.clientY;
+	        
+	        // grab the clicked element's position
+	        _offsetX = ExtractNumber(target.style.left);
+	        _offsetY = ExtractNumber(target.style.top);
+	        
+	        // bring the clicked element to the front while it is being dragged
+	        _oldZIndex = target.style.zIndex;
+	        target.style.zIndex = 10000;
+	        
+	        // we need to access the element in OnMouseMove
+	        _dragElement = target;
+
+	        // tell our code to start moving the element with the mouse
+	        document.onmousemove = OnMouseMove;
+	        
+	        // cancel out any text selections
+	        document.body.focus();
+
+	        // prevent text selection in IE
+	        document.onselectstart = function () { return false; };
+	        // prevent IE from trying to drag an image
+	        target.ondragstart = function() { return false; };
+	        
+	        // prevent text selection (except IE)
+	        return false;
+	    }
+	}
+
+	function OnMouseMove(e)
+	{
+	    if (e == null) 
+	        var e = window.event; 
+
+	    // this is the actual "drag code"
+	    _dragElement.style.left = (_offsetX + e.clientX - _startX) + 'px';
+	    _dragElement.style.top = (_offsetY + e.clientY - _startY) + 'px';
+	    
+	    _debug.innerHTML = '(' + _dragElement.style.left + ', ' + 
+	        _dragElement.style.top + ')';   
+	}
+
+	function OnMouseUp(e)
+	{
+	    if (_dragElement != null)
+	    {
+	        _dragElement.style.zIndex = _oldZIndex;
+
+	        // we're done with these events until the next OnMouseDown
+	        document.onmousemove = null;
+	        document.onselectstart = null;
+	        _dragElement.ondragstart = null;
+
+	        // this is how we know we're not dragging      
+	        _dragElement = null;
+	        
+	        _debug.innerHTML = 'mouse up';
+	    }
+	}
+
+	function ExtractNumber(value)
+	{
+	    var n = parseInt(value);
+		
+	    return n == null || isNaN(n) ? 0 : n;
+	}
+
+	// this is simply a shortcut for the eyes and fingers
+	function $(id)
+	{
+	    return document.getElementById(id);
+	}
+
 })(window,jQuery);
